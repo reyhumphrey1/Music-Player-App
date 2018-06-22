2e9bcf15232613e4b40db417abc56ccc19c9fb82
Rey Humphrey <reyhumphrey1@gmail.com>
2018-06-22 17:27:51 +0300

    Added the some text Views to display a song's length and name

--- app/src/main/java/com/dope/rey/musicplayerapp/MainActivity.java
+++ app/src/main/java/com/dope/rey/musicplayerapp/MainActivity.java
@@ -4,8 +4,13 @@ import android.media.MediaPlayer;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.SeekBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.concurrent.TimeUnit;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,6 +24,9 @@ public class MainActivity extends AppCompatActivity {
     private int forwardTime = 5000;
     private int backwardTime = 5000;
     private SeekBar seekbar;
+    private TextView startSongLength, songName, endSongLength;
+
+    private static int oneTimeOnly = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +37,39 @@ public class MainActivity extends AppCompatActivity {
         buttonPause = findViewById(R.id.button_pause);
         buttonBack = findViewById(R.id.button_back);
         buttonRewind = findViewById(R.id.button_rewind);
+
+        startSongLength = findViewById(R.id.start_song_length);
+        songName = findViewById(R.id.song_name);
+        endSongLength = findViewById(R.id.end_song_length);
+
+        songName.setText("Song.mp3");
+
+        mediaPlayer = MediaPlayer.create(this,R.raw.mankind_north);
+        seekbar = findViewById(R.id.seekBar);
+        seekbar.setClickable(false);
+        buttonPause.setEnabled(false);
+
+        buttonBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Toast.makeText(getApplicationContext(), "Now Playing Music", Toast.LENGTH_SHORT).show();
+                mediaPlayer.start();
+
+                finalTime = mediaPlayer.getDuration();
+                startTime = mediaPlayer.getCurrentPosition();
+
+                if(oneTimeOnly == 0){
+                    seekbar.setMax((int)finalTime);
+                    oneTimeOnly = 1;
+                }
+
+                seekbar.setProgress((int) startTime);
+               // myhandler.postDelayed(UpdateSongTime, 100);
+                buttonPause.setEnabled(false);
+                buttonBack.setEnabled(true);
+            }
+        });
 
 
     }
